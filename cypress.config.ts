282ef736f6e6e2eb7fd535607df15d1e53ed773a
annarhughes <annaraehughes@live.co.uk>
2026-03-04 14:53:12 +0100

    fix package lock

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  // Register at https://cloud.cypress.io to obtain a projectId.
+  // Set CYPRESS_PROJECT_ID in .env.local and in GitHub Actions secrets.
+  projectId: process.env.CYPRESS_PROJECT_ID,
+
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.ts',
