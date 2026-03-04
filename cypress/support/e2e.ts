a61fc68d39721653fcd0dd9916757aebaf325b84
annarhughes <annaraehughes@live.co.uk>
2026-03-04 15:30:10 +0100

    rm wait-for-vercel-preview

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,9 +1,1 @@
 import './commands'
-
-if (Cypress.env('VERCEL_BYPASS_SECRET')) {
-  beforeEach(() => {
-    cy.intercept('**', (req) => {
-      req.headers['x-vercel-protection-bypass'] = Cypress.env('VERCEL_BYPASS_SECRET')
-    })
-  })
-}
