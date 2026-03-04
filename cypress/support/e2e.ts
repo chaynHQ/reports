fd45fa9f63227ea7dba38202a2c66b93cd5ff1e9
annarhughes <annaraehughes@live.co.uk>
2026-03-04 15:25:56 +0100

    fix vercel token

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,1 +1,9 @@
 import './commands'
+
+if (Cypress.env('VERCEL_BYPASS_SECRET')) {
+  beforeEach(() => {
+    cy.intercept('**', (req) => {
+      req.headers['x-vercel-protection-bypass'] = Cypress.env('VERCEL_BYPASS_SECRET')
+    })
+  })
+}
