36ff7009880f8fd861f2f1d1b2c7d66c98ad027a
annarhughes <annaraehughes@live.co.uk>
2026-03-04 15:39:08 +0100

    VERCEL_BYPASS_SECRET

--- cypress/support/e2e.ts
+++ cypress/support/e2e.ts
@@ -1,1 +1,11 @@
-import './commands'
+import "./commands";
+
+if (Cypress.env("VERCEL_BYPASS_SECRET")) {
+  beforeEach(() => {
+    cy.intercept("**", (req) => {
+      req.headers["x-vercel-protection-bypass"] = Cypress.env(
+        "VERCEL_BYPASS_SECRET",
+      );
+    });
+  });
+}
